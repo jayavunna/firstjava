70bcdddf82477e7f3af75e9f995f6db10dc0d3af
Jaya Vunna <you@example.com>
2019-01-10 19:04:54 +0000

    flskdjfdslkf

---
+++ src/loveProgramming/javafirstproject.java
@@ -0,0 +1,9 @@
+package loveProgramming;
+
+public class javafirstproject {
+    public static void main(String[] args) {
+        System.out.println("Hello Jaya,this is the first java program!");
+
+
+    }
+}
